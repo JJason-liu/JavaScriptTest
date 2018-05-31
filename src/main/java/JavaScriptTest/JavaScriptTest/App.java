4853c85e37a185d17bdd14c172dddecb344e7548
Administrator <Administrator@PC-20180420QNNV>
2018-05-31 10:08:08 +0800

    首次提交

---
+++ src/main/java/JavaScriptTest/JavaScriptTest/App.java
@@ -0,0 +1,13 @@
+package JavaScriptTest.JavaScriptTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
